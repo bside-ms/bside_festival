8abe11451556e5002375b848181c6bf66e364d9c
Carsten Dietzel <carsten.dietzel@check24.de>
2022-05-20 18:00:39 +0200

    Add instagram and facebook to footer

--- components/common/Footer.tsx
+++ components/common/Footer.tsx
@@ -1,24 +1,47 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import Link from 'next/link';
 import type { ReactElement } from 'react';
+import { faFacebookSquare, faInstagram } from '@fortawesome/free-brands-svg-icons';
 
 const Footer = (): ReactElement => {
 
-    const link1 = (
+    const imprintLink = (
         <Link href="https://b-side.ms/kv/impressum/">
             <a className="underline">Impressum</a>
         </Link>
     );
-    const link2 = (
+    const privacyPolicyLink = (
         <Link href="https://b-side.ms/kv/datenschutz/">
-            <a className="underline">Datenschutzerklärung</a>
+            <a className="underline">Datenschutz</a>
+        </Link>
+    );
+
+    const instagramLink = (
+        <Link href="https://www.instagram.com/bsidemuenster/">
+            <a className="p-3 text-xl" target="_blank">
+                <FontAwesomeIcon icon={faInstagram} />
+            </a>
+        </Link>
+    );
+
+    const facebookLink = (
+        <Link href="https://www.facebook.com/bsidemuenster">
+            <a className="p-3 text-xl" target="_blank">
+                <FontAwesomeIcon icon={faFacebookSquare} />
+            </a>
         </Link>
     );
 
     return (
         <div className="text-sm text-gray-400 text-center p-4 bg-gray-800">
             <div>Veranstaltet durch den B-Side Kultur e.V.</div>
+
             <div className="mt-1 flex gap-3 justify-center">
-                <span>{link1}</span> <span>{link2}</span>
+                <span>{instagramLink}</span> <span>{facebookLink}</span>
+            </div>
+
+            <div className="mt-1 flex gap-3 justify-center">
+                <span>{imprintLink}</span> <span>{privacyPolicyLink}</span>
             </div>
         </div>
     );
