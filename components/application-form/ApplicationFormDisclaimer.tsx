47b64f428e19dda483fee94c7b44220cf9b80762
Carsten Dietzel <carsten.dietzel@check24.de>
2022-06-09 18:38:26 +0200

    Add disclaimer for family and misc

--- components/application-form/ApplicationFormDisclaimer.tsx
+++ components/application-form/ApplicationFormDisclaimer.tsx
@@ -55,9 +55,29 @@ const ApplicationFormDisclaimer = ({ applicationType }: Props): ReactElement | n
             );
 
         case ApplicationType.familienprogramm:
+        case ApplicationType.anderes:
+            return (
+                <div className=" text-[#3a1a85] text-sm space-y-3 mt-4">
+                    <div>
+                        Das B-Side Festival ist auch 2022 ein Festival für alle mit vielfältigem und
+                        buntem Programm. Dabei wollen wir insbesondere Räume und Bühnen für FLINTA*
+                        und andere marginalisierte Gesellschaftsgruppen schaffen.
+                    </div>
+
+                    <div>
+                        Das B-Side Festival wird nicht kommerziell, ohne Eintrittsgelder und im Sinne
+                        der Gemeinnützigkeit für die Allgemeinheit frei zugänglich veranstaltet. Das
+                        Festival wird auch dieses Jahr wieder durch öffentliche Fördermittel, Spenden
+                        und den Eigenanteil des B-Side Kultur e.V. als Veranstalter finanziert. Im
+                        Rahmen unserer finanziellen Möglichkeiten erhalten Kunst- und Kulturveranstaltungen
+                        und Angebote des Bildungsprogramms eine finanzielle Förderung. (Materialkosten
+                        und Fahrtkosten können nach vorheriger Absprache erstattet werden).
+                    </div>
+                </div>
+            );
+
         case ApplicationType.essensstand:
         case ApplicationType.nachbarschaft:
-        case ApplicationType.anderes:
             return null;
     }
 
