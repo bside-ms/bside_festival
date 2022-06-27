5a6a93547d9e44720219201ba4cce655b47a17db
Carsten Dietzel <carsten.dietzel@check24.de>
2022-06-27 22:19:06 +0200

    Undo timezone fixing, luckily I was wrong, this is only a problem on a local dev server - which actually makes sense

--- lib/applications/useApplicationCreatedDate.ts
+++ lib/applications/useApplicationCreatedDate.ts
@@ -1,18 +1,7 @@
-import { addHours } from 'date-fns';
 import useFormattedDate from 'lib/common/useFormattedDate';
 
-const useApplicationCreatedDate = (createdAt: string): string => {
-
-    /**
-     * I know about time zones and stuff like that, but for some reason we have to
-     * correct the creation date here. Our server's time is UTC and the creation date
-     * in the database will be prefilled by MySQL with the current timestamp. But
-     * somewhere between reading the database values and sending them to the client,
-     * someone decides to subtract another two hours..
-     */
-    const correctedDate = addHours(new Date(createdAt), 2);
-
-    return useFormattedDate(correctedDate, 'dd.MM.yyyy, HH:mm \'Uhr\'');
-};
+const useApplicationCreatedDate = (createdAt: string): string => (
+    useFormattedDate(new Date(createdAt), 'dd.MM.yyyy, HH:mm \'Uhr\'')
+);
 
 export default useApplicationCreatedDate;
