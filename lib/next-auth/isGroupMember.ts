824220da678cfa2a02dea31fe6a32a26b058f316
Carsten Dietzel <carsten.dietzel@check24.de>
2022-08-03 09:13:18 +0200

    Undo debug code

--- lib/next-auth/isGroupMember.ts
+++ lib/next-auth/isGroupMember.ts
@@ -1,22 +1,21 @@
 import type { Session } from 'next-auth';
+import isExtendedSession from 'lib/next-auth/isExtendedSession';
 
 const isGroupMember = (group: string, session: Session | null | undefined): boolean => {
 
-    return true;
+    if (session === null || session === undefined) {
+        return false;
+    }
 
-    // if (session === null || session === undefined) {
-    //     return false;
-    // }
-    //
-    // if (!isExtendedSession(session) || session.user === undefined) {
-    //     return false;
-    // }
-    //
-    // if (session.user.keycloakGroups === undefined) {
-    //     return false;
-    // }
-    //
-    // return session.user.keycloakGroups.includes(group);
+    if (!isExtendedSession(session) || session.user === undefined) {
+        return false;
+    }
+
+    if (session.user.keycloakGroups === undefined) {
+        return false;
+    }
+
+    return session.user.keycloakGroups.includes(group);
 };
 
 export default isGroupMember;
