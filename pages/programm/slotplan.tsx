19a13c898b29e5270d6be0053e27c4c4987f0502
Carsten Dietzel <carsten.dietzel@check24.de>
2022-08-04 09:42:32 +0200

    Remove WIP context changes

--- pages/programm/slotplan.tsx
+++ pages/programm/slotplan.tsx
@@ -2,7 +2,6 @@ import { useSession } from 'next-auth/react';
 import type { ReactElement } from 'react';
 import ApplicationsOverviewNotAllowed from 'components/applications/ApplicationsOverviewNotAllowed';
 import TimeTable from 'components/program/timeTable/TimeTable';
-import TimeTableContextWrapper from 'components/program/timeTable/TimeTableContextWrapper';
 import SwrResponseWrapper from 'components/strapi/SwrResponseWrapper';
 import isGroupMember from 'lib/next-auth/isGroupMember';
 import type AllFullTimeProgramItemsResponse from 'lib/strapi/typings/AllFullTimeProgramItemsResponse';
@@ -29,26 +28,24 @@ export default (): ReactElement => {
     return (
         <div className="min-h-screen pt-8">
             <div className="w-full pl-5 mx-auto relative">
-                <TimeTableContextWrapper>
-                    <SwrResponseWrapper<AllFullTimeProgramItemsResponse> response={swrFullTimeProgramItemsResponse}>
-                        {({ allFullTimeProgramItems: { exhibitions, foods, informationBooths } }): ReactElement => (
-                            <SwrResponseWrapper<AllProgramItemsResponse> response={swrProgramItemsResponse}>
-                                {({ allProgramItems: { concerts, workshops, performances, readings, familyPrograms } }): ReactElement => (
-                                    <TimeTable
-                                        concerts={concerts ?? []}
-                                        workshops={workshops ?? []}
-                                        performances={performances ?? []}
-                                        readings={readings ?? []}
-                                        familyPrograms={familyPrograms ?? []}
-                                        exhibitions={exhibitions ?? []}
-                                        foods={foods ?? []}
-                                        informationBooths={informationBooths ?? []}
-                                    />
-                                )}
-                            </SwrResponseWrapper>
-                        )}
-                    </SwrResponseWrapper>
-                </TimeTableContextWrapper>
+                <SwrResponseWrapper<AllFullTimeProgramItemsResponse> response={swrFullTimeProgramItemsResponse}>
+                    {({ allFullTimeProgramItems: { exhibitions, foods, informationBooths } }): ReactElement => (
+                        <SwrResponseWrapper<AllProgramItemsResponse> response={swrProgramItemsResponse}>
+                            {({ allProgramItems: { concerts, workshops, performances, readings, familyPrograms } }): ReactElement => (
+                                <TimeTable
+                                    concerts={concerts ?? []}
+                                    workshops={workshops ?? []}
+                                    performances={performances ?? []}
+                                    readings={readings ?? []}
+                                    familyPrograms={familyPrograms ?? []}
+                                    exhibitions={exhibitions ?? []}
+                                    foods={foods ?? []}
+                                    informationBooths={informationBooths ?? []}
+                                />
+                            )}
+                        </SwrResponseWrapper>
+                    )}
+                </SwrResponseWrapper>
             </div>
         </div>
     );
