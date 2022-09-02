13ba6704cb858866d9e564cbece4403fa48b2032
Carsten Dietzel <carsten.dietzel@check24.de>
2022-09-02 16:24:39 +0200

    Show description in program for full-time items

--- components/program/program/FullTimeProgramItemDetails.tsx
+++ components/program/program/FullTimeProgramItemDetails.tsx
@@ -1,5 +1,7 @@
 import { Chip } from '@mui/material';
 import type { ReactElement } from 'react';
+import ArtistDescription from 'components/artist/ArtistDescription';
+import getDetailsFromProgramItem from 'lib/strapi/getDetailsFromProgramItem';
 import type FullTimeProgramItem from 'lib/strapi/typings/FullTimeProgramItem';
 
 interface Props {
@@ -7,6 +9,8 @@ interface Props {
 }
 
 const FullTimeProgramItemDetails = ({ programItem }: Props): ReactElement => {
+
+    const { artist } = getDetailsFromProgramItem(programItem);
 
     const location = programItem.attributes.location.data?.attributes.Name ?? null;
 
@@ -32,6 +36,17 @@ const FullTimeProgramItemDetails = ({ programItem }: Props): ReactElement => {
                     </div>
                 )}
             </div>
+
+            {artist !== null && (
+                <>
+                    <div className="mt-2 hidden md:block">
+                        <ArtistDescription artist={artist} truncateAfterLines={5} />
+                    </div>
+                    <div className="mt-2 md:hidden">
+                        <ArtistDescription artist={artist} truncateAfterLines={2} />
+                    </div>
+                </>
+            )}
         </div>
     );
 };
