f191eeaad936b64823a9dd2d2b78282fe7a286c2
Kennet <kennet@seebruecke.org>
2022-09-05 22:56:33 +0200

    Revert to 5395d560

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,28 +1,13 @@
 import '../styles/globals.css';
 
-import { type ReactElement, useEffect } from 'react';
 import { RouterScrollProvider } from '@moxy/next-router-scroll';
-import { init } from '@socialgouv/matomo-next';
 import type { AppProps } from 'next/app';
 import { SessionProvider } from 'next-auth/react';
+import type { ReactElement } from 'react';
 import NextHead from 'components/common/NextHead';
 import { NavigationOverlayContextProvider } from 'components/navigation/NavigationOverlayContext';
 
-const MATOMO_URL = process.env.NEXT_PUBLIC_MATOMO_URL;
-const MATOMO_SITE_ID = process.env.NEXT_PUBLIC_MATOMO_SITE_ID;
-
 const MyApp = ({ Component, pageProps }: AppProps): ReactElement => {
-    useEffect(() => {
-        if (MATOMO_URL === undefined || MATOMO_SITE_ID === undefined) {
-            return;
-        }
-        init({
-            url: MATOMO_URL,
-            siteId: MATOMO_SITE_ID,
-            jsTrackerFile: 'lernen.js',
-            phpTrackerFile: 'lernen.php',
-        });
-    }, []);
 
     return (
         // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
