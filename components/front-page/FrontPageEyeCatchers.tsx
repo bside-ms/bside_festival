75aadff63f631f6ef6ddce42d4a25b8e84d850eb
Carsten Dietzel <carsten.dietzel@check24.de>
2022-09-22 08:17:27 +0200

    Remove some obsolete stuff; show team picture on front page

--- components/front-page/FrontPageEyeCatchers.tsx
+++ components/front-page/FrontPageEyeCatchers.tsx
@@ -1,8 +1,7 @@
 import styles from './FrontPageEyeCatchers.module.scss';
 
-import Link from 'next/link';
+import Image from 'next/image';
 import type { ReactElement } from 'react';
-import Button from 'components/common/Button';
 import ContentWrapper from 'components/common/ContentWrapper';
 import BHeartGrid from 'components/front-page/BHeartGrid';
 import BSideInfo from 'components/front-page/BSideInfo';
@@ -15,19 +14,18 @@ const FrontPageEyeCatchers = (): ReactElement => {
                 <div className="relative">
                     <div className={styles.box}>
                         <div className={`${styles.boxContent ?? ''} space-y-2`}>
-                            <div>
-                                Endlich geht es wieder los: Das B-Side Festival kehrt zurück
-                            </div>
-
                             <div className={`${styles.title ?? ''} font-display`}>
-                                Zurück für die Zukunft!
+                                Das B-Side Festival-Team sagt Danke!
                             </div>
 
                             <div>
-                                Nachdem wir 2021 wegen der Corona-Pandemie einen Pause eingelegt haben, freuen wir
-                                uns umso mehr auf diesen September. Denn vom 16. bis zum 18.09.2022 werden die
-                                Straßen, Kneipen und Hinterhöfe des Hansaviertels wieder mit Kunst, Musik,
-                                Workshops und Theater gefüllt.
+                                Danke an alle ehrenamtlichen Helfer:innen, an alle Unterstützenden
+                                aus dem Viertel und aus der B-Side. Danke an alle, die sich beworben
+                                haben und danke an alle, die dieses unfassbar vielfältige Programm
+                                mitgestaltet haben. Danke an alle Locations und Kooperationspartner:innen.
+                                Und natürlich Danke an alle Menschen, die unser Festival besucht haben!
+                                Wir sind unfassbar glücklich, dass dieses Wochenende dank euch allen
+                                möglich war.
                             </div>
                         </div>
 
@@ -44,25 +42,28 @@ const FrontPageEyeCatchers = (): ReactElement => {
             </ContentWrapper>
 
             <ContentWrapper>
-                <div className={`flex flex-col gap-3 ${styles.bigBox ?? ''}`}>
-                    <div>
-                        Schon sehr bald geht es im Hansaviertel rund! Für unser B-Side Festival 2022
-                        „Zurück für die Zukunft“ brauchen wir euch! Wir suchen tatkräftige Helfer*innen. Dabei gibt
-                        es verschiedene Aufgaben, bei denen ihr euch einbringen könnt: Die Betreuung von  Konzerten,
-                        Workshops, Ausstellungen und Lesungen, die Verpflegung für das Helfer*innen- und B-Side-Team,
-                        Hilfe beim Auf- und Abbau und der Technik des Festivals, Unterstützung des Awareness-Teams
-                        auf dem gesamten Festival und beim Spendensammeln.
+                <div className="flex flex-col gap-3 mt-[40px] py-[25px] px-[40px] bg-gradient-to-b from-[#2c9fc9] to-[#e1017e] text-white">
+                    <div className="group">
+                        <div className="group-hover:hidden">
+                            <Image
+                                src="/assets/images/festival/team1.jpg"
+                                width={2992}
+                                height={2607}
+                                layout="responsive"
+                            />
+                        </div>
+                        <div className="hidden group-hover:block">
+                            <Image
+                                src="/assets/images/festival/team2.jpg"
+                                width={2992}
+                                height={2607}
+                                layout="responsive"
+                            />
+                        </div>
                     </div>
                     <div>
-                        Mit allen Stärken und Interessen seid ihr herzlich willkommen uns zu unterstützen! Denn nur
-                        mit euch kann das Festival so toll wie die letzten Jahre werden!
-                    </div>
-                    <div>
-                        <Link href="/mithelfen" passHref={true}>
-                            <Button>
-                                Jetzt mithelfen
-                            </Button>
-                        </Link>
+                        Bald findet ihr an dieser Stelle viele tolle Fotos und noch mehr vom Festival! Also schaut
+                        bald mal wieder vorbei!
                     </div>
                 </div>
             </ContentWrapper>
