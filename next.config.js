bce9c729cd282d046488b22ea213be54a9f85bd0
Carsten Dietzel <carsten.dietzel@check24.de>
2023-03-07 11:56:09 +0100

    Redirect every route to the front page

--- next.config.js
+++ next.config.js
@@ -1,24 +1,27 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
-  reactStrictMode: true,
-  experimental: {
-    scrollRestoration: true
-  },
-  async redirects() {
+    reactStrictMode: true,
+    experimental: {
+        scrollRestoration: true
+    },
+    async redirects() {
 
-    // This is used so the images of the leaflet map are shown, not sure
-    // how to tell leaflet to use a different path for its images instead.
-    return [
-      { source: '/layers-2x.png', destination: '/assets/leaflet/layers-2x.png', permanent: true },
-      { source: '/layers.png', destination: '/assets/leaflet/layers.png', permanent: true },
-      { source: '/marker-icon-2x.png', destination: '/assets/leaflet/marker-icon-2x.png', permanent: true },
-      { source: '/marker-icon.png', destination: '/assets/leaflet/marker-icon.png', permanent: true },
-      { source: '/marker-shadow.png', destination: '/assets/leaflet/marker-shadow.png', permanent: true },
-      { source: '/orte/layers-2x.png', destination: '/assets/leaflet/layers-2x.png', permanent: true },
-      { source: '/orte/layers.png', destination: '/assets/leaflet/layers.png', permanent: true },
-      { source: '/orte/marker-icon-2x.png', destination: '/assets/leaflet/marker-icon-2x.png', permanent: true },
-      { source: '/orte/marker-icon.png', destination: '/assets/leaflet/marker-icon.png', permanent: true },
-      { source: '/orte/marker-shadow.png', destination: '/assets/leaflet/marker-shadow.png', permanent: true },
-    ]
-  }
+        return [
+            // Temporarily redirect every route to the front page
+            { source: '/(.+)', destination: '/', permanent: false },
+
+            // This is used so the images of the leaflet map are shown, not sure
+            // how to tell leaflet to use a different path for its images instead.
+            { source: '/layers-2x.png', destination: '/assets/leaflet/layers-2x.png', permanent: true },
+            { source: '/layers.png', destination: '/assets/leaflet/layers.png', permanent: true },
+            { source: '/marker-icon-2x.png', destination: '/assets/leaflet/marker-icon-2x.png', permanent: true },
+            { source: '/marker-icon.png', destination: '/assets/leaflet/marker-icon.png', permanent: true },
+            { source: '/marker-shadow.png', destination: '/assets/leaflet/marker-shadow.png', permanent: true },
+            { source: '/orte/layers-2x.png', destination: '/assets/leaflet/layers-2x.png', permanent: true },
+            { source: '/orte/layers.png', destination: '/assets/leaflet/layers.png', permanent: true },
+            { source: '/orte/marker-icon-2x.png', destination: '/assets/leaflet/marker-icon-2x.png', permanent: true },
+            { source: '/orte/marker-icon.png', destination: '/assets/leaflet/marker-icon.png', permanent: true },
+            { source: '/orte/marker-shadow.png', destination: '/assets/leaflet/marker-shadow.png', permanent: true },
+        ]
+    }
 };
