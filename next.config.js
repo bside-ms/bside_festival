f8b4d5df5617ad80c8054dd3c8b62c63cdc011dc
Carsten Dietzel <carsten.dietzel@check24.de>
2023-06-04 12:47:45 +0200

    Add fallback for env

--- next.config.js
+++ next.config.js
@@ -4,8 +4,8 @@ module.exports = {
     images: {
         remotePatterns: [
             {
-                protocol: "https",
-                hostname: process.env.NEXT_PUBLIC_IONOS_HOST_NAME,
+                protocol: 'https',
+                hostname: process.env.NEXT_PUBLIC_IONOS_HOST_NAME ?? '',
             },
         ],
     },
