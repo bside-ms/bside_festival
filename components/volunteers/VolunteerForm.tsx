acbed1227c8f6210ece1fc559ab5d17bfc26c5ac
Adrian <adrianbueno095@gmail.com>
2023-06-22 21:28:43 +0200

    create a new volunters component, add a new volunteerFrom.tsx file

---
+++ components/volunteers/VolunteerForm.tsx
@@ -0,0 +1,12 @@
+import type { ReactElement } from 'react';
+
+const VolunteerForm = (): ReactElement => {
+
+    return (
+        <>
+           
+        </>
+    );
+};
+
+export default VolunteerForm;
