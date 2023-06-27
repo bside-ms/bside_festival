ab386f821b010871b0665ffea3883add44531207
Adrian <adrianbueno095@gmail.com>
2023-06-27 21:38:58 +0200

    new text in the Volunteer form

--- components/volunteers/volunteerForm/VolunteerForm.tsx
+++ components/volunteers/volunteerForm/VolunteerForm.tsx
@@ -2,6 +2,7 @@
 import { type ReactElement, useCallback } from 'react';
 import { FormProvider, useForm } from 'react-hook-form';
 import type { ApplicationFormValues } from 'components/applications/applicationForm/ApplicationForm';
+import ContentWrapper from 'components/common/ContentWrapper';
 
 // do i also need a type? 
 export interface VolunteerFormValues {
@@ -31,46 +32,82 @@ const VolunteerForm = (): ReactElement => {
     const handleFormReset = useCallback(() => reset(), [reset]);
 
     // what should i watch here?,
-    const currentType = watch('');
+    const currentType = watch('type');
 
     const handleFormSubmit = useCallback(async (values: ApplicationFormValues) => {
         // What does root do? i couldnt find any information about it. 
         clearErrors('root');
         
-    // const request = {
-    // type: values.type,    
-    // fullName: values.fullName,
-    // mailAddress: values.mailAddress,
-    // phoneNumber: values.photoNumber,
-    // canCook: values.canCook,
-    // hasCar: values.hasCar,
-    // hasDrivingLicense: values.hasDrivingLicense,
-    // canCarryHeavyStuff: values.canCarryHeavyStuff,
-    // canLiftHeavyStuff: values.canLiftHeavyStuff,
-    // isSocial: values.isSocial,
-    // canSupportTechnician: values.canSupportTechnician,
-    // canSupportArtist: values.canSupportArtist,
-    // hasMultipleTalents: values.hasMultipleTalents,
-    // canWorkWithChildren: values.canWorkWithChildren,
-    // canCleanupAfterShow: values.canCleanupAfterShow,
-    // isAvailableOnFriday: values.isAvailableOnFriday,
-    // isAvailableOnSaturday: values.isAvailableOnSaturday,
+        // const request = {
+        // type: values.type,    
+        // fullName: values.fullName,
+        // mailAddress: values.mailAddress,
+        // phoneNumber: values.photoNumber,
+        // canCook: values.canCook,
+        // hasCar: values.hasCar,
+        // hasDrivingLicense: values.hasDrivingLicense,
+        // canCarryHeavyStuff: values.canCarryHeavyStuff,
+        // canLiftHeavyStuff: values.canLiftHeavyStuff,
+        // isSocial: values.isSocial,
+        // canSupportTechnician: values.canSupportTechnician,
+        // canSupportArtist: values.canSupportArtist,
+        // hasMultipleTalents: values.hasMultipleTalents,
+        // canWorkWithChildren: values.canWorkWithChildren,
+        // canCleanupAfterShow: values.canCleanupAfterShow,
+        // isAvailableOnFriday: values.isAvailableOnFriday,
+        // isAvailableOnSaturday: values.isAvailableOnSaturday,
 
-    // };
+        // };
 
     }, [clearErrors, handleFormReset, setError]);
 
     return (
-        <FormProvider {...methods}>
-            <div className ="w-full">
-                <form
-                onSubmit={handleSubmit(handleFormSubmit)}
-                noValidate={true}
-                className="flex gap-6 flex-col"
-                >
-                <div>Hello World</div>
+        <>
+            <div className="bg-blue">
+                <ContentWrapper>
+                    <div className="text-2xl">
+                        Mithelfen
+                    </div>
+                </ContentWrapper>
             </div>
-        </FormProvider>
+
+            <div className="bg-blue">
+                <ContentWrapper>
+                    <div>
+                        <div>
+                            Schon sehr bald geht es im Hansaviertel rund! Für unser B-Side Festival 2023
+                            „Interaktion Reaktion“ brauchen wir euch! Wir suchen tatkräftige Helfer*innen. Dabei gibt
+                            es verschiedene Aufgaben, bei denen ihr euch einbringen könnt: Die Betreuung von  Konzerten,
+                            Workshops, Ausstellungen und Lesungen, die Verpflegung für das Helfer*innen- und B-Side-Team,
+                            Hilfe beim Auf- und Abbau und der Technik des Festivals, Unterstützung des Awareness-Teams
+                            auf dem gesamten Festival und beim Spendensammeln.
+                        </div>
+                        <div>
+                            Mit allen Stärken und Interessen seid ihr herzlich willkommen uns zu unterstützen! Denn nur
+                            mit euch kann das Festival so toll wie die letzten Jahre werden!
+                        </div>
+                        <div>
+                            Um einen Überblick über eure Stärken und Interessen zu bekommen,
+                            haben wir folgenden Fragebogen vorbereitet. Kreuzt bitte an,
+                            was auf euch zutrifft. Außerdem hinterlasst bitte eure Kontaktdaten,
+                            sodass wir euch erreichen können. Danke!
+                        </div>
+                    </div>
+                
+                </ContentWrapper>
+            </div>
+            <FormProvider {...methods}>
+                <div className="w-full">
+                    <form
+                        // onSubmit={handleSubmit(handleFormSubmit)}
+                        noValidate={true}
+                        className="flex gap-6 flex-col"
+                    >
+                        <div>Hello World</div>
+                    </form>
+                </div>
+            </FormProvider>
+        </>
     );
 };
 
