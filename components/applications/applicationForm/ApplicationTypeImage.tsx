35fcf5e8d00167227121d1c1a138aff45f021cb5
Carsten Dietzel <carsten.dietzel@check24.de>
2023-06-28 15:51:21 +0200

    Improve type images

--- components/applications/applicationForm/ApplicationTypeImage.tsx
+++ components/applications/applicationForm/ApplicationTypeImage.tsx
@@ -11,19 +11,19 @@ const ApplicationTypeImage = ({ type }: Props): ReactElement => {
 
     switch (type) {
         case Type.Reading:
-            return <Image src="/assets/images/types/reading.jpg" alt={typeLabels.Reading} fill={true} className="object-cover" />;
+            return <Image src="/assets/images/types/reading.jpeg" alt={typeLabels.Reading} fill={true} className="object-cover" />;
         case Type.FamilyProgram:
-            return <Image src="/assets/images/types/familyProgram.jpg" alt={typeLabels.FamilyProgram} fill={true} className="object-cover" />;
+            return <Image src="/assets/images/types/familyProgram.jpeg" alt={typeLabels.FamilyProgram} fill={true} className="object-cover" />;
         case Type.Exhibition:
-            return <Image src="/assets/images/types/exhibition.jpg" alt={typeLabels.Exhibition} fill={true} className="object-cover" />;
+            return <Image src="/assets/images/types/exhibition.jpeg" alt={typeLabels.Exhibition} fill={true} className="object-cover" />;
         case Type.Workshop:
-            return <Image src="/assets/images/types/workshop.jpg" alt={typeLabels.Workshop} fill={true} className="object-cover" />;
+            return <Image src="/assets/images/types/workshop.jpeg" alt={typeLabels.Workshop} fill={true} className="object-cover" />;
         case Type.Performance:
-            return <Image src="/assets/images/types/performance.jpg" alt={typeLabels.Performance} fill={true} className="object-cover" />;
+            return <Image src="/assets/images/types/performance.jpeg" alt={typeLabels.Performance} fill={true} className="object-cover" />;
         case Type.Concert:
-            return <Image src="/assets/images/types/concert.jpg" alt={typeLabels.Concert} fill={true} className="object-cover" />;
+            return <Image src="/assets/images/types/concert.jpeg" alt={typeLabels.Concert} fill={true} className="object-cover" />;
         default:
-            return <Image src="/assets/images/types/concert.jpg" alt="Bewerbung" fill={true} className="object-cover" />;
+            return <Image src="/assets/images/types/misc.jpeg" alt="Bewerbung" fill={true} className="object-cover" />;
     }
 };
 
