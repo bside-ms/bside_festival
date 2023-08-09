bcb36b66ba7a39236ee5d6f21de85d342beef174
Carsten Dietzel <carsten.dietzel@check24.de>
2023-08-10 01:13:30 +0200

    Add hint

--- components/volunteers/volunteersOverview/VolunteersOverview.tsx
+++ components/volunteers/volunteersOverview/VolunteersOverview.tsx
@@ -1,4 +1,5 @@
 import type { Volunteer } from '@prisma/client';
+import Link from 'next/link';
 import { useSession } from 'next-auth/react';
 import type { ReactElement } from 'react';
 import VolunteerDetails from 'components/volunteers/volunteersOverview/VolunteerDetails';
@@ -15,10 +16,25 @@ const VolunteersOverview = ({ volunteers }: Props): ReactElement => {
 
     const isInDataPrivacyGroup = isGroupMember('/kreise/festival/eingeschränkt/datenschutz', session);
 
+    const portalLink = (
+        <Link
+            href="https://portal.b-side.ms/group/284ef230-f3f0-4368-a895-334ed02ff59a"
+            className="underline cursor-pointer"
+            target="_blank"
+        >
+            Datenschutz-Gruppe
+        </Link>
+    );
+
     return (
         <div>
             <div className="text-3xl mb-5 font-display">
                 Helfer:innen ({volunteers.length})
+            </div>
+
+            <div className="rounded-md drop-shadow bg-gray-50 p-2 mb-3">
+                Die Kontakt-Daten der Helfer:innen sind nur für Mitglieder
+                der {portalLink} des Festivals sichtbar.
             </div>
 
             <div className="grid grid-cols-1 gap-3">
