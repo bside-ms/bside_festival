435312f9e0e35fdf6e23f10f4cbb4ed9b245c47e
Carsten Dietzel <carsten.dietzel@check24.de>
2023-09-13 20:47:24 +0200

    Update everything

--- lib/upload/createPutObjectCommand.ts
+++ lib/upload/createPutObjectCommand.ts
@@ -1,12 +1,8 @@
 import process from 'process';
 import { PutObjectCommand } from '@aws-sdk/client-s3';
-import type { PutObjectCommandInputType } from '@aws-sdk/client-s3/dist-types/commands/PutObjectCommand';
+import type { PutObjectCommandInput } from '@aws-sdk/client-s3/dist-types/commands/PutObjectCommand';
 
-const createPutObjectCommand = (
-    objectKey: string,
-    objectContentType: string,
-    object: PutObjectCommandInputType['Body'],
-): PutObjectCommand => {
+const createPutObjectCommand = (objectKey: string, objectContentType: string, object: PutObjectCommandInput['Body']): PutObjectCommand => {
     return new PutObjectCommand({
         Bucket: process.env.NEXT_PUBLIC_IONOS_BUCKET_NAME,
         Key: objectKey,
