f1a1f114bc92410a486ec7e6e2dda165929fe3e8
Carsten Dietzel <carsten.dietzel@check24.de>
2023-09-14 21:18:51 +0200

    Add programmheft

--- components/participants/overview/ParticipantsOverview.tsx
+++ components/participants/overview/ParticipantsOverview.tsx
@@ -1,19 +1,13 @@
-import { useCallback, useState } from 'react';
-import { faMapLocationDot } from '@fortawesome/free-solid-svg-icons';
+import { faFilePdf, faMapLocationDot } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import Image from 'next/image';
 import Link from 'next/link';
 import type { ReactElement } from 'react';
 import ParticipantOverview from 'components/participants/overview/ParticipantOverview';
 import ParticipantsOverviewAdditionalFilters from 'components/participants/overview/ParticipantsOverviewAdditionalFilters';
 import { useParticipantsOverviewContext } from 'components/participants/overview/ParticipantsOverviewContext';
 import ParticipantsOverviewTypesFilter from 'components/participants/overview/ParticipantsOverviewTypesFilter';
-import typeLabels from 'lib/participants/typeLabels';
 
 const ParticipantsOverview = (): ReactElement => {
-    const [showMap, setShowMap] = useState(false);
-    const toggleShowMap = useCallback(() => setShowMap((prevState) => !prevState), []);
-
     const { filteredParticipants } = useParticipantsOverviewContext();
 
     return (
@@ -29,28 +23,22 @@ const ParticipantsOverview = (): ReactElement => {
 
             <ParticipantsOverviewAdditionalFilters />
 
-            <div className="mt-5 mb-2">
-                <a
-                    onClick={toggleShowMap}
+            <div className="mt-5 mb-4 flex gap-4">
+                <Link
+                    href="/assets/map.svg"
+                    target="_blank"
                     className="inline-flex gap-2 items-center border border-gray-900 px-3 py-1 rounded-full cursor-pointer"
                 >
                     <FontAwesomeIcon className="w-5" icon={faMapLocationDot} /> Lageplan
-                </a>
+                </Link>
 
-                {showMap && (
-                    <div className="p-5">
-                        <div className="bg-white bg-opacity-50 p-10 w-full md:w-1/2 max-w-xl">
-                            <Image
-                                src="/assets/map.svg"
-                                alt={typeLabels.Reading}
-                                width="128"
-                                height="128"
-                                layout="responsive"
-                                className="object-cover"
-                            />
-                        </div>
-                    </div>
-                )}
+                <Link
+                    href="/assets/b-side-festival-2023-programm.pdf"
+                    target="_blank"
+                    className="inline-flex gap-2 items-center border border-gray-900 px-3 py-1 rounded-full cursor-pointer"
+                >
+                    <FontAwesomeIcon className="w-5" icon={faFilePdf} /> Programmheft
+                </Link>
             </div>
 
             <div className="grid grid-cols-1 gap-5">
