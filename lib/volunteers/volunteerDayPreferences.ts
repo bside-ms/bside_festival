4120a45fee09fc4cce4bd00987ac80de30301bf5
Carsten Dietzel <carsten.dietzel@check24.de>
2025-08-03 15:21:17 +0200

    Fix dates

--- lib/volunteers/volunteerDayPreferences.ts
+++ lib/volunteers/volunteerDayPreferences.ts
@@ -2,8 +2,8 @@ import type VolunteerPreference from './VolunteerPreference';
 
 const volunteerDayPreferences = new Array<VolunteerPreference>(
     { key: 'isAvailableBefore', label: 'Bereits vor dem Festival (Deko, etc.)', emoji: '📆' },
-    { key: 'isAvailableOnFriday', label: 'Freitag, 20.09.', emoji: '📆' },
-    { key: 'isAvailableOnSaturday', label: 'Samstag, 21.09.', emoji: '📆' },
+    { key: 'isAvailableOnFriday', label: 'Freitag, 19.09.', emoji: '📆' },
+    { key: 'isAvailableOnSaturday', label: 'Samstag, 20.09.', emoji: '📆' },
 );
 
 export default volunteerDayPreferences;
