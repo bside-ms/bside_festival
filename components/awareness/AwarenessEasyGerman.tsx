526598edf2b95043ef1023cf3eb3d0996d2d07e0
Carsten Dietzel <carsten.dietzel@check24.de>
2025-09-16 23:42:28 +0200

    Update AwarenessEasyGerman with refined text, clearer structure, and enhanced discrimination and safety guidelines

--- components/awareness/AwarenessEasyGerman.tsx
+++ components/awareness/AwarenessEasyGerman.tsx
@@ -3,216 +3,107 @@ import type { ReactElement } from 'react';
 const AwarenessEasyGerman = (): ReactElement => {
     return (
         <div className="flex flex-col gap-5 bg-white p-4 font-sans drop-shadow-2xl">
-            Wenn du auf dem Festival bist:
-            <br />
-            Und es dir nicht gut geht.
-            <br />
-            Oder wenn du Hilfe brauchst.
-            <br />
-            Dann kannst uns anrufen.
-            <br />
-            Dann kommen Menschen und helfen dir.
-            <br />
-            <br />
-            Willkommen auf dem B-Side Festival.
-            <br />
-            Hier sollen sich alle Menschen sicher fühlen.
-            <br />
-            Aber manchmal fühlen sich Menschen nicht sicher.
-            <br />
-            Auch wenn sich andere Menschen in der gleichen Situation sicher fühlen.
-            <br />
-            Das liegt daran, dass wir unterschiedliche Erfahrungen gemacht haben.
-            <br />
-            Manche Menschen wurden in der Vergangenheit schon schlecht behandelt.
-            <br />
-            Zum Beispiel:
-            <br />
-            Wegen ihres Geschlechts.
-            <br />
-            Oder weil sie eine Behinderung haben.
-            <br />
-            Oder weil sie lesbisch oder schwul sind.
-            <br />
-            Das schwierige Wort für schlecht behandeln heißt:
-            <br />
-            Diskriminieren.
-            <br />
-            <br />
-            Wir wollen, dass sich alle Menschen auf dem B-Side Festival sicher fühlen.
-            <br />
-            Deshalb gibt es ein paar Regeln:
-            <br />
-            <br />
-            <span className="text-xl">1.</span>
-            <br />
-            Jeder Mensch hat eigene Grenzen.
-            <br />
-            Grenzen sind Sachen, bei denen eine Person sagt: Nein.
-            <br />
-            Oder Sache, die ein Person nicht machen möchte. Oder sich dabei unwohl fühlt.
-            <br />
-            Manchmal sagt eine Person auch nicht Nein.
-            <br />
-            Aber meint es trotzdem.
-            <br />
-            Und fühlt sich unwohl.
-            <br />
-            Vielleicht traut sich eine Person nicht, Nein zu sagen.
-            <br />
-            Wenn sie etwas nicht machen möchte.
-            <br />
-            Manchmal machen andere Menschen trotzdem weiter.
-            <br />
-            Und hören nicht auf das Nein von der anderen Person.
-            <br />
-            Oder zwingen eine Person, etwas zu machen.
-            <br />
-            Dann sagt man:
-            <br />
-            Diese Person überschreitet eine Grenze.
-            <br />
-            <br />
-            Nur wenn eine Person wirklich sagt: Ja.
-            <br />
-            Dann bedeutet es, dass die Person eine Sache machen möchte.
-            <br />
-            Vielleicht weiß man nicht, ob eine Person sich unwohl fühlt.
-            <br />
-            Deshalb muss man nachfragen.
-            <br />
-            Wenn man sich nicht sicher ist, ob man nachfragen sollte.
-            <br />
-            Sollte man es trotzdem tun.
-            <br />
-            <br />
-            <span className="text-xl">2.</span>
-            <br />
-            Manchmal überschreiten Menschen die Grenzen von anderen Menschen.
-            <br />
-            Das passiert auch auf Festivals in Münster.
-            <br />
-            Auch wenn man denkt:
-            <br />
-            Hier auf diesem Festival passiert es nicht.
-            <br />
-            Weil so viele achtsame Menschen hier sind.
-            <br />
-            Und es ein Awareness-Team gibt.
-            <br />
-            <br />
-            <strong>Awareness heißt:</strong>
-            <br />
-            Aufmerksam sein.
-            <br />
-            Zu probieren, dass es allen gut geht.
-            <br />
-            Und die Bedürfnisse von anderen Personen beachten.
-            <br />
-            Manchmal werden Menschen schlecht behandelt.
-            <br />
-            Weil die Person anders ist.
-            <br />
-            Zum Beispiel:
-            <br />
-            Wegen ihres Geschlechts.
-            <br />
-            Oder wegen einer Behinderung.
-            <br />
-            Oder weil sie lesbisch oder schwul sind.
-            <br />
-            Das schwierige Wort für schlecht behandeln heißt:
-            <br />
-            Diskriminieren.
-            <br />
-            <br />
-            <span className="text-xl">3.</span>
-            <br />
-            Alle Menschen auf dem Festival müssen vorsichtig sein:
-            <br />
-            Damit es allen gut geht.
-            <br />
-            Und niemand diskriminiert wird.
-            <br />
-            Die Gäste müssen vorsichtig sein.
-            <br />
-            Und die Leute, die das Festival organisieren.
-            <br />
-            Und die Künstler und Künstlerinnen.
-            <br />
-            <br />
-            <strong>Wird eine Grenze von dir überschritten?</strong>
-            <br />
-            <strong>Wird die Grenze von jemand anderem überschritten?</strong>
-            <br />
-            <strong>Dann kannst du etwas machen:</strong>
-            <br />
-            <br />
-            <span className="text-xl">1.</span>
-            <br />
-            Bringe dich in Sicherheit.
-            <br />
-            Oder bringe die andere Person in Sicherheit.
-            <br />
-            Andere Personen sollst du immer Fragen.
-            <br />
-            Ob sie Hilfe möchten.
-            <br />
-            <br />
-            <span className="text-xl">2.</span>
-            <br />
-            Es gibt das Awareness-Team.
-            <br />
-            Das sind Menschen mit pinken Westen.
-            <br />
-            <br />
-            <img src="/assets/awareness_west.png" className="w-2/3 md:w-1/2" />
-            <br />
-            Oder Menschen mit pinken Arm-binden.
-            <br />
-            <br />
-            <img src="/assets/awareness_wristband.png" className="w-2/3 md:w-1/2" />
-            <br />
-            Sie können andere Menschen um Hilfe fragen.
-            <br />
-            Und sie können etwas machen:
-            <br />
-            Dass die Person, die dich schlecht behandelt, vom Festival weggeht.
-            <br />
-            <br />
-            <span className="text-xl tracking-wider">3.</span>
-            <br />
-            Du kannst die Awareness-Telefonnummer anrufen.
-            <br />
-            Dann kommt Hilfe.
-            <br />
-            Oder sagen dir, wo du hingehen kannst.
-            <br />
-            Damit du in Sicherheit bist.
-            <br />
-            <br />
-            <span className="text-xl">4.</span>
-            <br />
-            Du kannst zum Awareness-Treffpunkt gehen.
-            <br />
-            Der Treffpunkt ist vor dem Haus der B-Side.
-            <br />
-            Die Adresse ist Am Hawerkamp 29.
-            <br />
-            Dort siehst du ein Schild, auf dem <strong>Awareness</strong> steht.
-            <br />
-            <br />
-            <strong>Wichtig:</strong>
-            <br />
-            Das Awareness-Team ist nicht der Notarzt.
-            <br />
-            Wenn es jemandem körperlich sehr schlecht geht:
-            <br />
-            Musst du den Notarzt anrufen.
-            <br />
-            Die Nummer vom Notarzt ist:
-            <br />
-            <strong>112</strong>
+            <div>
+                <div className="mb-1 text-lg font-bold">Menschen, die dir helfen können</div>
+                <div>
+                    Beim B-Side Festival gibt es ein Team, das auf dich aufpasst. Dieses Team hilft dir, wenn jemand gemein zu dir ist
+                    oder du dich nicht wohlfühlst.
+                </div>
+                <ul className="list-outside list-disc space-y-2 pl-7">
+                    <li className="pl-2">Du erkennst das Team an den leuchtend pinken Westen.</li>
+                    <li className="pl-2">Du kannst jederzeit mit ihnen sprechen, wenn du dich nicht gut fühlst oder Fragen hast.</li>
+                    <li className="pl-2">Du findest das Team im B-Side Wohnzimmer. Dort kannst du mit ihnen reden.</li>
+                    <li className="pl-2">Wenn du einen ruhigen Ort brauchst, bringen sie dich dorthin.</li>
+                    <li className="pl-2">Awareness-Telefon: 01788887435</li>
+                </ul>
+            </div>
+
+            <div>
+                <div>
+                    Wir möchten, dass alle Menschen freundlich, respektvoll und hilfsbereit sind. Alle sollen sich wohl fühlen. Niemand soll
+                    verletzt oder schlecht behandelt werden. Deshalb gibt es Regeln für alle. Alle sollen auf sich und auf andere achten.
+                </div>
+            </div>
+
+            <div>
+                <div className="mb-1 font-bold">Hier sind die wichtigsten Regeln</div>
+
+                <div className="space-y-3">
+                    <div>
+                        <div className="underline">Persönliche Grenzen</div>
+                        <div>
+                            Jeder Mensch hat eigene Grenzen. Was für eine Person okay ist, ist für eine andere Person vielleicht nicht okay.
+                            Respektiere die Grenzen von anderen.
+                        </div>
+                        <ul className="list-outside list-disc space-y-1 pl-7">
+                            <li className="pl-2">Nur ein „Ja“ ist ein Ja.</li>
+                            <li className="pl-2">Frag lieber noch einmal nach, bevor du etwas machst.</li>
+                            <li className="pl-2">Bitte behalte deine Kleidung an, damit sich niemand unwohl fühlt.</li>
+                        </ul>
+                    </div>
+
+                    <div>
+                        <div className="underline">Diskriminierung und Grenzen überschreiten</div>
+                        <div>
+                            Diskriminierung bedeutet: Menschen werden schlecht behandelt, weil sie anders sind. Das ist nicht erlaubt. Gründe
+                            können sein: Herkunft, Geschlecht, Alter, Hautfarbe, Sexualität, Gesundheit oder Besitz.
+                        </div>
+                        <div>Grenzüberschreitungen sind zum Beispiel:</div>
+                        <ul className="list-outside list-disc space-y-1 pl-7">
+                            <li className="pl-2">Unangenehme Sprüche</li>
+                            <li className="pl-2">Ungewolltes Anfassen</li>
+                            <li className="pl-2">Zu persönliche Fragen</li>
+                            <li className="pl-2">Andere ausschließen</li>
+                        </ul>
+                        <div>So etwas tolerieren wir nicht und werden, wenn möglich, dagegen vorgehen.</div>
+                    </div>
+
+                    <div>
+                        <div className="underline">Respekt für Kulturen</div>
+                        <div>
+                            Zeige Verständnis und Respekt für die Bräuche und Traditionen anderer Kulturen. Nimm keine Symbole oder
+                            Gegenstände aus anderen Kulturen, um sie zu benutzen oder zu zeigen. Tu das nicht, wenn du die Bedeutung nicht
+                            kennst. Das nennt man „Aneignung“. Das ist nicht erlaubt.
+                        </div>
+                    </div>
+
+                    <div>
+                        <div className="underline">Geschlechter und Pronomen</div>
+                        <ul className="list-outside list-disc space-y-1 pl-7">
+                            <li className="pl-2">Stecke Menschen nicht wegen ihres Aussehens in Schubladen.</li>
+                            <li className="pl-2">Frage, wie die Person genannt werden möchte (z. B. „sie“, „er“, „they“, „Name“).</li>
+                            <li className="pl-2">Respektiere die Wünsche der Person.</li>
+                        </ul>
+                    </div>
+
+                    <div>
+                        <div className="underline">Selbst-Fürsorge</div>
+                        <ul className="list-outside list-disc space-y-1 pl-7">
+                            <li className="pl-2">Pass auf dich auf.</li>
+                            <li className="pl-2">Wenn du dich unsicher fühlst, sprich mit Freund*innen darüber.</li>
+                            <li className="pl-2">Schütze dein Getränk vor K.-o.-Tropfen. Nutze einen Deckel und lass dein Getränk nicht aus den Augen.</li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+
+            <div>
+                <div className="mb-1 font-bold">Was tun, wenn du etwas Unangenehmes siehst oder erlebst?</div>
+                <ol className="list-outside list-decimal space-y-2 pl-7">
+                    <li className="pl-2">
+                        <span className="font-semibold">Hilf den Betroffenen.</span> Zeige, dass du da bist, und biete Hilfe an. Höre zu und
+                        glaube der Person. Frage, was die Person braucht.
+                    </li>
+                    <li className="pl-2">
+                        <span className="font-semibold">Wende dich an das Awareness-Team, die Theke oder das Sicherheitspersonal.</span> Das Team
+                        trägt pinke Westen. Du kannst sie ansprechen, wenn du Hilfe brauchst. Du kannst auch anrufen: 01788887435.
+                    </li>
+                </ol>
+            </div>
+
+            <div className="italic">
+                Wichtig: Das Awareness-Team ersetzt nicht den Rettungsdienst. In Notfällen rufe bitte den Notruf 112 an.
+            </div>
         </div>
     );
 };
