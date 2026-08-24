fc354088699144da19b3701b2b3c03163c4bc24b
Carsten Dietzel <carsten.dietzel@check24.de>
2026-08-24 08:55:18 +0200

    Remove obsolete files

--- prisma/migrations/20260822121000_drop_updated_description/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `updatedDescription` on the `Participant` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE `Participant` DROP COLUMN `updatedDescription`;
